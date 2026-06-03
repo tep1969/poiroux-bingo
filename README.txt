195199a5c52dc267889bc9dfc6755fb91a399dd1
tep1969 <poirouxt@bellsouth.net>
2026-06-03 13:55:17 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,12 @@
+Annual Poiroux Reunion Bingo PWA
+
+How to use on iPad/iPhone:
+1. Host this folder on HTTPS or localhost.
+2. Open index.html in Safari.
+3. Use Share -> Add to Home Screen.
+4. Launch from the Home Screen icon.
+5. After the first load, it works offline.
+
+Important:
+- Service workers do not run from file:// URLs on iPad or iPhone.
+- This version does not include printing.
---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE_NAME = 'poiroux-bingo-v1';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))); self.clients.claim(); });
+self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => { const clone = resp.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone)).catch(()=>{}); return resp; }).catch(() => caches.match('./index.html')))); });
